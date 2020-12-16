28f6e72662d09d089120294e2a0841968a2085be
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-12-16 18:19:08 +0100

    Component MainLayout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import styles from './App.scss';
-import List from '../List/ListContainer.js';
-import Creator from '../Creator/Creator';
-import PropTypes from 'prop-types';
-import {Search} from '../Search/SearchContainer.js';
+import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
-class App extends React.Component {
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array,
-  }
-  render() {
-    const { title, subtitle, lists } = this.props;
-    return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search/>
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-        <div className={styles.creator}>
-          <Creator action={title => this.addList(title)}/>
-        </div>	          
-      </main>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
